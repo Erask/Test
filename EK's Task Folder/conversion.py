bba5fbc02202fae42516bc9e1c28cae90f9f61e2
Erastka <88916598+Erask@users.noreply.github.com>
2024-12-13 01:51:12 +0000

    initial commit

---
+++ EK's Task Folder/conversion.py
@@ -0,0 +1,27 @@
+#Variable declarations 
+num1 = 99.23
+
+num2 = 23
+
+num3 = 150
+
+string1 = "100"
+
+#Converting the num1 a float into an integer 
+integer_value = int(num1)
+
+#Integer output 
+print(integer_value)
+
+#Converting num2 integer into a float 
+
+float_value = float(num2)
+
+#Output the converted num2 integer into a float
+print(float_value)
+
+#Converting string1 string into an integer
+integer_value = int(string1) 
+
+#Output the coverted string1 string to an integer
+print(string1)
---
+++ EK's Task Folder/hello_world.py
@@ -0,0 +1,11 @@
+# Asking user's to input their name
+name = input("Enter your name: ")
+#output of the input 
+print(f"Your name is {name}")
+
+# Taking user's age as input and printing it
+age = input("Enter your age: ")
+print(f"Your age is {age}")
+
+# output of hello world as the next line of code after print completion above
+print("hello World")
